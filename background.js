42bafd773830a21d9f94ad1a3514d732382f4977
mvs <malte.lispel@gmail.com>
2016-12-11 22:59:31 +0100

    Add files via upload

--- background.js
+++ background.js
@@ -1,17 +1,36 @@
 var on = true;
 var current = 0;
-// Called when the user clicks on the browser action.
+var hide = function(){
+    chrome.tabs.insertCSS(null, {file: "style.css"});
+};
+var setIcon = function(){
+    chrome.browserAction.setIcon({path:"icon" + current + ".png"});
+};
+
+//Set icon when extension is laoded
+setIcon();
+
+//When new page is loaded check if "on" is false
+chrome.webNavigation.onCompleted.addListener(function(details) {
+  if (on == false) {
+    hide();
+  }
+});
+
+// Called when the user clicks on the extension icon
 chrome.browserAction.onClicked.addListener(function(tab) {
-  // No tabs or host permissions needed!
+    // Hide
     if (on) {
-        chrome.tabs.insertCSS(null, {file: "style.css"});
+        hide();
         on = false;
         current = 1;
     }
+    //Show
     else {
         chrome.tabs.insertCSS(null, {file: "style2.css"});
         on = true;
         current = 0;
     }
-    chrome.browserAction.setIcon({path:"icon" + current + ".png"});
+    //Set icon
+    setIcon();
 });
